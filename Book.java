d8e43381e5e11a82cc3e447736bb5ea5deae99d6
tyebii <tyler.biesemeyer2022@gmail.com>
2023-10-07 19:46:59 -0700

    added basic User/Book framework and createList method start

--- Book.java
+++ Book.java
@@ -1,6 +1,21 @@
 
 public class Book {
 
-    //test 123
+    private int ISBN;
+    private String title;
+    private String author;
+
+
+    enum Status {
+        CHECKED_IN,
+        CHECKED_OUT
+    }
+
+    public Book(String t, String a, int num) {
+        title = t;
+        author = a;
+        ISBN = num;
+    }
+
     
 }
--- User.java
+++ User.java
@@ -1,4 +1,29 @@
+import com.sun.management.OperatingSystemMXBean;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.*;
 
 public class User {
-    
+
+    private String userName;
+    private int libraryCard;
+    private Book[] borrowedBooks;
+
+    public void createList(String filename) throws FileNotFoundException {
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+
+        // tbd line-by-line -> book objects
+
+    }
+
+    public void borrowBook(){
+
+    }
+
+    public void returnBook(){
+
+    }
 }
